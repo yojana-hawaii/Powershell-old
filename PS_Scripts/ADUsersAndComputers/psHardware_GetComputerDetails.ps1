55b83c1ef96e149ac9bdb28e56b030b75cf1befb
yojana <yojana.hawaii@gmail.com>
2022-08-02 14:59:49 -1000

    refactor service and isLaptop

--- PS_Scripts/ADUsersAndComputers/psHardware_GetComputerDetails.ps1
+++ PS_Scripts/ADUsersAndComputers/psHardware_GetComputerDetails.ps1
@@ -1,3 +1,4 @@
+
 function fnLocal_GetServiceStatus{
     param($pComputerName, $pServiceName)
     try {
@@ -7,6 +8,19 @@ function fnLocal_GetServiceStatus{
         write-host $pComputerName "missing" $pServiceName
     }
     return $service
+}
+function fnLocal_isLaptop($ComputerName){
+
+    $isLaptop = 0
+    $chasis_type = Get-WmiObject -class win32_systemenclosure -computerName $ComputerName | select-object chassistypes
+    $battery = Get-WmiObject -class win32_battery -ComputerName $ComputerName 
+    # $battery
+
+    if ($chasis_type.chassistypes -eq 9 -or $chasis_type.chassistypes -eq 10 -or $chasis_type.chassistypes -eq 14 -or $battery ){
+        $isLaptop = 1
+    }
+    
+    return $isLaptop
 }
 function fnLocal_GetLocalComputerDetails($pComputer){
     $localCompName = $pComputer.Name
@@ -20,32 +34,28 @@ function fnLocal_GetLocalComputerDetails($pComputer){
         $AnyPatch = Get-HotFix  -ComputerName $localCompName | Sort-Object InstalledOn -Descending | Select-Object -First 1 
         write-host "patch complete"
 
-        $print_service = fnLocal_GetServiceStatus -pComputerName $localCompName -pServiceName "Spooler"
-        $kace_service = fnLocal_GetServiceStatus -pComputerName $localCompName -pServiceName "konea"
-        $sysaid_service = fnLocal_GetServiceStatus -pComputerName $localCompName -pServiceName "SysAidAgent"
-        $sentinel_service = fnLocal_GetServiceStatus -pComputerName $localCompName -pServiceName "SentinelAgent"
-        $dellEncryption_service = fnLocal_GetServiceStatus -pComputerName $localCompName -pServiceName "DellMgmtAgent"
-        $cylance_service = fnLocal_GetServiceStatus -pComputerName $localCompName -pServiceName "CylanceSvc"
-        write-host "service complete"
+        $PSCustom_CompDetails = @()
+        $PSCustom_CompDetails = [PSCustomObject]@{
+            Name = $localCompName
+            sAMAccountName = $lComputerSMA
+
+            Last_Security_KB = $securityPatch.HotFixID
+            Last_SecurityPatch_date = $securityPatch.InstalledOn
+            LastPatchKb = $AnyPatch.HotFixID
+            LastPatchDate = $AnyPatch.InstalledOn
+
+            Print_Status = fnLocal_GetServiceStatus -pComputerName $localCompName -pServiceName "Spooler"
+            Kace_status = fnLocal_GetServiceStatus -pComputerName $localCompName -pServiceName "konea"
+            Sentinel_Status = fnLocal_GetServiceStatus -pComputerName $localCompName -pServiceName "SysAidAgent"
+            Sysaid_Status = fnLocal_GetServiceStatus -pComputerName $localCompName -pServiceName "SentinelAgent"
+            DellEncryption_Status = fnLocal_GetServiceStatus -pComputerName $localCompName -pServiceName "DellMgmtAgent"
+            Cylance_Status = fnLocal_GetServiceStatus -pComputerName $localCompName -pServiceName "CylanceSvc"
+
+            IsLaptop = fnLocal_isLaptop($localCompName)
+        } 
     }
 
-    $PSCustom_CompDetails = @()
-    $PSCustom_CompDetails = [PSCustomObject]@{
-        Name = $localCompName
-        sAMAccountName = $lComputerSMA
-
-        Last_Security_KB = $securityPatch.HotFixID
-        Last_SecurityPatch_date = $securityPatch.InstalledOn
-        LastPatchKb = $AnyPatch.HotFixID
-        LastPatchDate = $AnyPatch.InstalledOn
-
-        Print_Status = $print_service
-        Kace_status = $kace_service
-        Sentinel_Status = $sentinel_service
-        Sysaid_Status = $sysaid_service
-        DellEncryption_Status = $dellEncryption_service
-        Cylance_Status = $cylance_service
-    } 
+    
     return $PSCustom_CompDetails
 }
 function fnHardware_GetManualComputerDetails($pComputerList){
