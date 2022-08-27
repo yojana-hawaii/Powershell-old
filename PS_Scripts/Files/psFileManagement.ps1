5923cd34fede6ad970d7e20a43b12448a8d822aa
yojana <yojana.hawaii@gmail.com>
2022-08-26 14:24:59 -1000

    zip yearly folder 2 months after the next year

--- PS_Scripts/Files/psFileManagement.ps1
+++ PS_Scripts/Files/psFileManagement.ps1
@@ -31,3 +31,26 @@ function fnFileMgmt_FileByYearMonthDay {
 
 }
 
+function fnFileMgmt_ZipOldFolder{
+    $today_year = Get-Date -Format yyyy 
+    $today_month = Get-Date -Format MM
+    $monthsToWaitBeforeArchiveAndDelete = 2
+
+    Get-ChildItem -Directory -Path $hl7FileLocation | ForEach-Object {
+        write-host $_.Name $today_year
+        if($_.Name -as [int] -lt $today_year -as [int]){
+            if($today_month -as [int] -gt $monthsToWaitBeforeArchiveAndDelete ){
+                write-host "ready for zip" $_.FullName
+                $zipFileNam = $_.FullName + ".zip"
+                Compress-Archive -Path $_.FullName -DestinationPath  $zipFileNam
+            }
+            else {"month not ready"}
+        }
+        else {"year not ready"}
+    }
+
+    
+
+}
+
+# fnFileMgmt_ZipOldFolder
